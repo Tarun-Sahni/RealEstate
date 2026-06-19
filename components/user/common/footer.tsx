0fc832f01573083fca0d4c1e7aa9145d334adaca
Tarun-Sahni <uservictim42@gmail.com>
2026-06-19 15:52:11 +0530

    update

--- components/user/common/footer.tsx
+++ components/user/common/footer.tsx
@@ -6,21 +6,21 @@ import { FaFacebook, FaInstagram, FaWhatsapp, FaYoutube } from 'react-icons/fa6'
 
 const Footer = () => {
   return (
-    <footer className='w-full bg-slate-200 dark:bg-muted px-4'>
+    <footer className='w-full bg-neutral-900 px-4'>
       <div className='max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-3 items-stretch pt-16 pb-8 gap-8'>
         <div className='flex flex-col gap-8'>
           <div className='max-w-sm'>
             <Logo width={100} height={100} />
-            <p className='text-sm tracking-wider'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis perferendis fugit veniam ad cumque labore suscipit distinctio non cupiditate nihil?</p>
+            <p className='text-sm tracking-wider text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis perferendis fugit veniam ad cumque labore suscipit distinctio non cupiditate nihil?</p>
           </div>
           <div className='flex flex-col md:flex-row gap-4'>
             <div className='flex flex-col justify-center items-start gap-1'>
-              <h2 className='text-muted-foreground text-xl font-playfair tracking-wider'>Customer Care</h2>
-              <Link href={`tel: ${TELEPHONE}`} className='text-sm hover:underline tracking-wider hover:text-yellow-600 hover:dark:text-yellow-500'>{TELEPHONE}</Link>
+              <h2 className='text-gray-500 text-xl font-playfair tracking-wider'>Customer Care</h2>
+              <Link href={`tel: ${TELEPHONE}`} className='text-sm hover:underline tracking-wider text-white hover:text-yellow-600 hover:dark:text-yellow-500'>{TELEPHONE}</Link>
             </div>
             <div className='flex flex-col justify-center items-start gap-1'>
-              <h2 className='text-muted-foreground text-xl font-playfair tracking-wider'>Live Support</h2>
-              <Link href={`mailto: ${MAIL}`} className='text-sm hover:underline tracking-wider hover:text-yellow-600 hover:dark:text-yellow-500'>{MAIL}</Link>
+              <h2 className='text-gray-500 text-xl font-playfair tracking-wider'>Live Support</h2>
+              <Link href={`mailto: ${MAIL}`} className='text-sm text-white hover:underline tracking-wider hover:text-yellow-600 hover:dark:text-yellow-500'>{MAIL}</Link>
             </div>
           </div>
           <div className='flex flex-col gap-4'>
@@ -29,28 +29,28 @@ const Footer = () => {
               <Link
                 href="https://facebook.com"
                 target="_blank"
-                className="text-black dark:text-white hover:text-[#1877F2] dark:hover:text-[#1877F2] transition-colors"
+                className="text-white hover:text-[#1877F2] dark:hover:text-[#1877F2] transition-colors"
               >
                 <FaFacebook className="size-6" />
               </Link>
               <Link
                 href="https://instagram.com"
                 target="_blank"
-                className="text-black dark:text-white hover:text-[#E4405F] dark:hover:text-[#E4405F] transition-colors"
+                className="text-white hover:text-[#E4405F] dark:hover:text-[#E4405F] transition-colors"
               >
                 <FaInstagram className="size-6" />
               </Link>
               <Link
                 href="https://youtube.com"
                 target="_blank"
-                className="text-black dark:text-white hover:text-[#FF0000] dark:hover:text-[#FF0000] transition-colors"
+                className="text-white hover:text-[#FF0000] dark:hover:text-[#FF0000] transition-colors"
               >
                 <FaYoutube className="size-7" />
               </Link>
               <Link
                 href="https://wa.me/919999999999"
                 target="_blank"
-                className="text-black dark:text-white hover:text-[#25D366] dark:hover:text-[#25D366] transition-colors"
+                className="text-white hover:text-[#25D366] dark:hover:text-[#25D366] transition-colors"
               >
                 <FaWhatsapp className="size-6" />
               </Link>
@@ -61,48 +61,23 @@ const Footer = () => {
           <div className='flex flex-col gap-4'>
             <h2 className='text-muted-foreground text-xl tracking-wider font-playfair'>Quick Search</h2>
             <div className='flex flex-col gap-4'>
-              <Link href="/" className='flex flex-row items-center gap-2 text-sm tracking-wider hover:translate-x-2 transition-all'>
+              <Link href="/" className='flex flex-row items-center gap-2 text-sm tracking-wider hover:translate-x-2 transition-all text-white'>
                 <ArrowRight strokeWidth={2.75} size={16} color="#eab308" />
                 Home
               </Link>
-              <Link href="/" className='flex flex-row items-center gap-2 text-sm tracking-wider hover:translate-x-2 transition-all'>
+              <Link href="/" className='flex flex-row items-center gap-2 text-sm tracking-wider hover:translate-x-2 transition-all text-white'>
                 <ArrowRight strokeWidth={2.75} size={16} color="#eab308" />
                 Home
               </Link>
-              <Link href="/" className='flex flex-row items-center gap-2 text-sm tracking-wider hover:translate-x-2 transition-all'>
+              <Link href="/" className='flex flex-row items-center gap-2 text-sm tracking-wider hover:translate-x-2 transition-all text-white'>
                 <ArrowRight strokeWidth={2.75} size={16} color="#eab308" />
                 Home
               </Link>
-              <Link href="/" className='flex flex-row items-center gap-2 text-sm tracking-wider hover:translate-x-2 transition-all'>
+              <Link href="/" className='flex flex-row items-center gap-2 text-sm tracking-wider hover:translate-x-2 transition-all text-white'>
                 <ArrowRight strokeWidth={2.75} size={16} color="#eab308" />
                 Home
               </Link>
-              <Link href="/" className='flex flex-row items-center gap-2 text-sm tracking-wider hover:translate-x-2 transition-all'>
-                <ArrowRight strokeWidth={2.75} size={16} color="#eab308" />
-                Home
-              </Link>
-            </div>
-          </div>
-          <div className='flex flex-col gap-4'>
-            <h2 className='text-muted-foreground text-xl tracking-wider font-playfair'>Quick Search</h2>
-            <div className='flex flex-col gap-4'>
-              <Link href="/" className='flex flex-row items-center gap-2 text-sm tracking-wider hover:translate-x-2 transition-all'>
-                <ArrowRight strokeWidth={2.75} size={16} color="#eab308" />
-                Home
-              </Link>
-              <Link href="/" className='flex flex-row items-center gap-2 text-sm tracking-wider hover:translate-x-2 transition-all'>
-                <ArrowRight strokeWidth={2.75} size={16} color="#eab308" />
-                Home
-              </Link>
-              <Link href="/" className='flex flex-row items-center gap-2 text-sm tracking-wider hover:translate-x-2 transition-all'>
-                <ArrowRight strokeWidth={2.75} size={16} color="#eab308" />
-                Home
-              </Link>
-              <Link href="/" className='flex flex-row items-center gap-2 text-sm tracking-wider hover:translate-x-2 transition-all'>
-                <ArrowRight strokeWidth={2.75} size={16} color="#eab308" />
-                Home
-              </Link>
-              <Link href="/" className='flex flex-row items-center gap-2 text-sm tracking-wider hover:translate-x-2 transition-all'>
+              <Link href="/" className='flex flex-row items-center gap-2 text-sm tracking-wider hover:translate-x-2 transition-all text-white'>
                 <ArrowRight strokeWidth={2.75} size={16} color="#eab308" />
                 Home
               </Link>
@@ -111,23 +86,48 @@ const Footer = () => {
           <div className='flex flex-col gap-4'>
             <h2 className='text-muted-foreground text-xl tracking-wider font-playfair'>Quick Search</h2>
             <div className='flex flex-col gap-4'>
-              <Link href="/" className='flex flex-row items-center gap-2 text-sm tracking-wider hover:translate-x-2 transition-all'>
+              <Link href="/" className='flex flex-row items-center gap-2 text-sm tracking-wider hover:translate-x-2 transition-all text-white'>
                 <ArrowRight strokeWidth={2.75} size={16} color="#eab308" />
                 Home
               </Link>
-              <Link href="/" className='flex flex-row items-center gap-2 text-sm tracking-wider hover:translate-x-2 transition-all'>
+              <Link href="/" className='flex flex-row items-center gap-2 text-sm tracking-wider hover:translate-x-2 transition-all text-white'>
                 <ArrowRight strokeWidth={2.75} size={16} color="#eab308" />
                 Home
               </Link>
-              <Link href="/" className='flex flex-row items-center gap-2 text-sm tracking-wider hover:translate-x-2 transition-all'>
+              <Link href="/" className='flex flex-row items-center gap-2 text-sm tracking-wider hover:translate-x-2 transition-all text-white'>
                 <ArrowRight strokeWidth={2.75} size={16} color="#eab308" />
                 Home
               </Link>
-              <Link href="/" className='flex flex-row items-center gap-2 text-sm tracking-wider hover:translate-x-2 transition-all'>
+              <Link href="/" className='flex flex-row items-center gap-2 text-sm tracking-wider hover:translate-x-2 transition-all text-white'>
                 <ArrowRight strokeWidth={2.75} size={16} color="#eab308" />
                 Home
               </Link>
-              <Link href="/" className='flex flex-row items-center gap-2 text-sm tracking-wider hover:translate-x-2 transition-all'>
+              <Link href="/" className='flex flex-row items-center gap-2 text-sm tracking-wider hover:translate-x-2 transition-all text-white'>
+                <ArrowRight strokeWidth={2.75} size={16} color="#eab308" />
+                Home
+              </Link>
+            </div>
+          </div>
+          <div className='flex flex-col gap-4'>
+            <h2 className='text-muted-foreground text-xl tracking-wider font-playfair'>Quick Search</h2>
+            <div className='flex flex-col gap-4'>
+              <Link href="/" className='flex flex-row items-center gap-2 text-sm tracking-wider hover:translate-x-2 transition-all text-white'>
+                <ArrowRight strokeWidth={2.75} size={16} color="#eab308" />
+                Home
+              </Link>
+              <Link href="/" className='flex flex-row items-center gap-2 text-sm tracking-wider hover:translate-x-2 transition-all text-white'>
+                <ArrowRight strokeWidth={2.75} size={16} color="#eab308" />
+                Home
+              </Link>
+              <Link href="/" className='flex flex-row items-center gap-2 text-sm tracking-wider hover:translate-x-2 transition-all text-white'>
+                <ArrowRight strokeWidth={2.75} size={16} color="#eab308" />
+                Home
+              </Link>
+              <Link href="/" className='flex flex-row items-center gap-2 text-sm tracking-wider hover:translate-x-2 transition-all text-white'>
+                <ArrowRight strokeWidth={2.75} size={16} color="#eab308" />
+                Home
+              </Link>
+              <Link href="/" className='flex flex-row items-center gap-2 text-sm tracking-wider hover:translate-x-2 transition-all text-white'>
                 <ArrowRight strokeWidth={2.75} size={16} color="#eab308" />
                 Home
               </Link>
@@ -136,14 +136,14 @@ const Footer = () => {
         </div>
       </div>
       {/* copyright */}
-      <div className='max-w-6xl mx-auto justify-between items-center border-t border-t-gray-500/20 py-8 flex flex-col md:flex-row gap-1'>
-        <p className='text-sm flex gap-1 text-wrap text-center md:text-left mx-auto md:m-0 tracking-wider'>&copy;{new Date().getFullYear()} <span className='text-yellow-500'>Gurgaon Elite Estate</span> - All Rights Reserved</p>
+      <div className='max-w-6xl mx-auto justify-between items-center border-t border-t-gray-500/50 py-8 flex flex-col md:flex-row gap-1'>
+        <p className='text-sm flex gap-1 text-wrap text-center md:text-left mx-auto md:m-0 tracking-wider text-white'>&copy;{new Date().getFullYear()} <span className='text-yellow-500'>Gurgaon Elite Estate</span> - All Rights Reserved</p>
         <div className='flex justify-center items-center gap-1'>
-          <Link href="/" className='text-xs tracking-widest capitalize cursor-pointer hover:bg-gray-300 hover:dark:bg-black px-3 py-1 rounded-full'>Privacy</Link>
+          <Link href="/" className='text-xs tracking-widest capitalize cursor-pointer hover:bg-gray-300 hover:dark:bg-black px-3 py-1 rounded-full text-white hover:text-black dark:text-white'>Privacy</Link>
           <Dot color='#eab308' />
-          <Link href="/" className='text-xs tracking-widest capitalize cursor-pointer hover:bg-gray-300 hover:dark:bg-black px-3 py-1 rounded-full'>Terms</Link>
+          <Link href="/" className='text-xs tracking-widest capitalize cursor-pointer hover:bg-gray-300 hover:dark:bg-black px-3 py-1 rounded-full text-white hover:text-black dark:text-white'>Terms</Link>
           <Dot color='#eab308' />
-          <Link href="/" className='text-xs tracking-widest capitalize cursor-pointer hover:bg-gray-300 hover:dark:bg-black px-3 py-1 rounded-full'>Sitemap</Link>
+          <Link href="/" className='text-xs tracking-widest capitalize cursor-pointer hover:bg-gray-300 hover:dark:bg-black px-3 py-1 rounded-full text-white hover:text-black dark:text-white'>Sitemap</Link>
         </div>
       </div>
     </footer>
--- components/user/common/header.tsx
+++ components/user/common/header.tsx
@@ -3,12 +3,22 @@ import { Button } from '@/components/ui/button'
 import Logo from './logo'
 import { Menu, User, X } from 'lucide-react'
 import Link from 'next/link'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const Header = () => {
     const [mobileNav, setMobileNav] = useState(false)
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => { setScrolled(window.scrollY >= window.innerHeight * 0.2) };
+        window.addEventListener("scroll", handleScroll);
+        return () => { window.removeEventListener("scroll", handleScroll) };
+    }, []);
     return (
-        <div className='w-full fixed z-50'>
+        <div className={`fixed top-0 left-0 right-0 z-50 transition-colors duration-300 ${scrolled
+            ? "bg-white dark:bg-black shadow-md"
+            : "bg-transparent"
+            }`}>
             <header className='w-full container mx-auto border-b border-b-gray-500/20 h-18 flex items-center justify-between px-4'>
                 <Logo />
                 {mobileNav ? (
@@ -21,19 +31,36 @@ const Header = () => {
                             </div>
                             <div className='w-full h-0.5 bg-gray-500/20 mb-4' />
                             <nav className='flex flex-col justify-start items-center gap-4'>
-                                <Link href="/" className='text-white px-4 py-1.5 transition-all tracking-widest text-sm hover:bg-gray-500/50 rounded-full'>Home</Link>
-                                <Link href="/" className='text-white px-4 py-1.5 transition-all tracking-widest text-sm hover:bg-gray-500/50 rounded-full'>About Us</Link>
-                                <Link href="/" className='text-white px-4 py-1.5 transition-all tracking-widest text-sm hover:bg-gray-500/50 rounded-full'>Contact Us</Link>
-                                <Link href="/" className='text-white px-4 py-1.5 transition-all tracking-widest text-sm hover:bg-gray-500/50 rounded-full'>Careers</Link>
+                                <Link href="/"
+                                    className="px-4 py-1.5 transition-all tracking-widest text-sm hover:bg-yellow-500 rounded-full">
+                                    Home
+                                </Link>
+
                             </nav>
                         </div>
                     </div>
                 ) : (
                     <nav className='hidden lg:flex flex-row justify-between items-center gap-1'>
-                        <Link href="/" className='text-white px-4 py-1.5 transition-all tracking-widest text-sm hover:bg-gray-500/50 rounded-full'>Home</Link>
-                        <Link href="/" className='text-white px-4 py-1.5 transition-all tracking-widest text-sm hover:bg-gray-500/50 rounded-full'>About Us</Link>
-                        <Link href="/" className='text-white px-4 py-1.5 transition-all tracking-widest text-sm hover:bg-gray-500/50 rounded-full'>Contact Us</Link>
-                        <Link href="/" className='text-white px-4 py-1.5 transition-all tracking-widest text-sm hover:bg-gray-500/50 rounded-full'>Careers</Link>
+                        <Link href="/"
+                            className={`px-4 py-1.5 transition-all tracking-widest text-sm hover:bg-yellow-500 hover:dark:text-black rounded-full ${scrolled ? "text-black dark:text-white" : "text-white"}`}>
+                            Home
+                        </Link>
+                        <Link href="/"
+                            className={`px-4 py-1.5 transition-all tracking-widest text-sm hover:bg-yellow-500 hover:dark:text-black rounded-full ${scrolled ? "text-black dark:text-white" : "text-white"}`}>
+                            Home
+                        </Link>
+                        <Link href="/"
+                            className={`px-4 py-1.5 transition-all tracking-widest text-sm hover:bg-yellow-500 hover:dark:text-black rounded-full ${scrolled ? "text-black dark:text-white" : "text-white"}`}>
+                            Home
+                        </Link>
+                        <Link href="/"
+                            className={`px-4 py-1.5 transition-all tracking-widest text-sm hover:bg-yellow-500 hover:dark:text-black rounded-full ${scrolled ? "text-black dark:text-white" : "text-white"}`}>
+                            Home
+                        </Link>
+                        <Link href="/"
+                            className={`px-4 py-1.5 transition-all tracking-widest text-sm hover:bg-yellow-500 hover:dark:text-black rounded-full ${scrolled ? "text-black dark:text-white" : "text-white"}`}>
+                            Home
+                        </Link>
                     </nav>
                 )}
                 <div className='flex flex-row justify-center items-center gap-1'>
