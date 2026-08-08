80f25ca4ef35a14f9ff743a7c36552c77f2e4f72
Tarun-Sahni <uservictim42@gmail.com>
2026-08-08 09:08:01 +0530

    update

--- components/admin/layout/chart.tsx
+++ components/admin/layout/chart.tsx
@@ -59,7 +59,7 @@ const rangeLabels: Record<string, string> = {
 }
 
 export function ChartAreaInteractive({ data }: { data: ActivityDataPoint[] }) {
-  const [timeRange, setTimeRange] = React.useState("90d")
+  const [timeRange, setTimeRange] = React.useState("7d")
 
   const filteredData = data.slice(-rangeToDays[timeRange])
 
@@ -74,7 +74,7 @@ export function ChartAreaInteractive({ data }: { data: ActivityDataPoint[] }) {
         </div>
         <Select value={timeRange} onValueChange={setTimeRange}>
           <SelectTrigger
-            className="hidden w-[160px] rounded-lg sm:ml-auto sm:flex"
+            className="hidden w-40 rounded-lg sm:ml-auto sm:flex"
             aria-label="Select a value"
           >
             <SelectValue placeholder="Last 3 months" />
@@ -95,7 +95,7 @@ export function ChartAreaInteractive({ data }: { data: ActivityDataPoint[] }) {
       <CardContent className="px-2 pt-4 sm:px-6 sm:pt-6">
         <ChartContainer
           config={chartConfig}
-          className="aspect-auto h-[250px] w-full"
+          className="aspect-auto h-62.5 w-full"
         >
           <AreaChart data={filteredData}>
             <defs>
